877f24a6d61ba311137ea01075f6ec33a697855c
Wilson Becker <wbecker@vaco.com>
2020-06-15 09:41:19 -0400

    adding user_loader

--- scrapyr/routes.py
+++ scrapyr/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from scrapyr import app
 from scrapyr.forms import UploadForm
+from scrapyr import auth
 
 @app.route('/')
 def index():
