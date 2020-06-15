de250965ed6f105068a763f31e254f867072ecc0
Wilson Becker <wbecker@vaco.com>
2020-06-15 10:37:21 -0400

    adding request_loader

--- scrapyr/auth.py
+++ scrapyr/auth.py
@@ -13,6 +13,17 @@ def load_user(user_id):
     except:
         return None
 
+@login_manager.request_loader
+def load_user_from_request(request):
+
+    auth_str = request.headers.get('Authorization')
+    token = auth_str.split(' ')[1] if auth_str else ''
+    if token:
+        user_id = User.decode_token(token)
+        user = User.query.get(int(user_id))
+        if user:
+            return user
+    return None
 
 @login_manager.unauthorized_handler
 def unauthorized():
